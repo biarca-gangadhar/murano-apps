2876b8dcaecc761ef5c48127195a0a465af07e6b
Gangadhar <root@fuel.domain.tld>
2016-02-02 12:53:17 +0000

    adding ssl verification

--- Docker/Kubernetes/KubernetesCluster/package/Resources/scripts/auto_scale/scale.sh
+++ Docker/Kubernetes/KubernetesCluster/package/Resources/scripts/auto_scale/scale.sh
@@ -30,12 +30,37 @@ username=$(awk -F "=" '/^username/ {print $2}' $data_file)
 password=$(awk -F "=" '/^password/ {print $2}' $data_file)
 header="Content-type: application/json"
 
-# Get Keystone token
-token=`curl -s -d '{ "auth": {"tenantName": "'"$tenant"'", "passwordCredentials": {"username": "'"$username"'", "password": "'"$password"'"}}}' -H "$header" http://$OPENSTACK_IP:5000/v2.0/tokens | jq --raw-output '.access.token.id'`
-echo "Token: $token"
+# to check http or https
+function check-ssl()
+{
+    # $1 - IP-ADDR and $2 - PORT
+    IP=$1
+    PORT=$2
+    echo | openssl s_client -connect $IP:$PORT 2>/dev/null | openssl x509 -noout -hash &> /dev/null
+    if [ $? -eq 0 ] ; then
+        echo "https"
+    else
+        echo "http"
+    fi
+}
 
+PORT_KEYSTONE=5000
+PORT_MURANO=8082
+proto=`check-ssl $OPENSTACK_IP $PORT_KEYSTONE`
+KEYSTONE_URL="$proto://$OPENSTACK_IP:$PORT_KEYSTONE"
+proto=`check-ssl $OPENSTACK_IP $PORT_MURANO`
+MURANO_URL="$proto://$OPENSTACK_IP:$PORT_MURANO"
+
+
+# Get Keystone token
+token=`curl -s -d '{ "auth": {"tenantName": "'"$tenant"'", "passwordCredentials": {"username": "'"$username"'", "password": "'"$password"'"}}}' -H "$header" $KEYSTONE_URL/v2.0/tokens | jq --raw-output '.access.token.id'`
+if [ -z $token ]; then
+    echo "Token Error"
+    exit 1
+fi
+echo "Token: $token"
 # Get the k8s environment ID.
-envs=$(curl -s -H "X-Auth-Token: $token" http://$OPENSTACK_IP:8082/v1/environments)
+envs=$(curl -s -H "X-Auth-Token: $token" $MURANO_URL/v1/environments)
 total_envs=`echo $envs | jq ".environments |  length"`
 count=0
 while [ $count -lt $total_envs ]; do
@@ -53,7 +78,7 @@ if [ -z "$env_id" ] ; then
 fi
 
 echo "Env ID: $env_id"
-services=$(curl -s -H "X-Auth-Token: $token" http://$OPENSTACK_IP:8082/v1/environments/$env_id/services)
+services=$(curl -s -H "X-Auth-Token: $token" $MURANO_URL/v1/environments/$env_id/services)
 
 # Get action Id's in easy and dirty way
 scaleUp=`echo $services | grep -o '[a-z0-9-]*_scaleNodesUp'`
@@ -64,18 +89,18 @@ scaleGceDown=`echo $services | grep -o '[a-z0-9-]*_deleteGceNode'`
 #  up
 if [ -z $gcp ] && [ $action == "up" ] ; then
     echo "Action ID: $scaleUp"
-    task=$(curl -s -H "X-Auth-Token: $token" -H "Content-Type: application/json" -d "{}" http://$OPENSTACK_IP:8082/v1/environments/$env_id/actions/$scaleUp)
+    task=$(curl -s -H "X-Auth-Token: $token" -H "Content-Type: application/json" -d "{}" $MURANO_URL/v1/environments/$env_id/actions/$scaleUp)
 elif [ -z $gcp ] && [ $action == "down" ] ; then
     echo "Action ID: $scaleDown"
-    task=$(curl -s -H "X-Auth-Token: $token" -H "Content-Type: application/json" -d "{}" http://$OPENSTACK_IP:8082/v1/environments/$env_id/actions/$scaleDown)
+    task=$(curl -s -H "X-Auth-Token: $token" -H "Content-Type: application/json" -d "{}" $MURANO_URL/v1/environments/$env_id/actions/$scaleDown)
 elif [ $gcp == "gce" ] && [ $action == "up" ]; then
     echo 1 > $AUTO_FLAG_FILE
     echo "Action ID: $scaleGceUp"
-    task=$(curl -s -H "X-Auth-Token: $token" -H "Content-Type: application/json" -d "{}" http://$OPENSTACK_IP:8082/v1/environments/$env_id/actions/$scaleGceUp)
+    task=$(curl -s -H "X-Auth-Token: $token" -H "Content-Type: application/json" -d "{}" $MURANO_URL/v1/environments/$env_id/actions/$scaleGceUp)
 elif [ $gcp == "gce" ] && [ $action == "down" ]; then
     echo 1 > $AUTO_FLAG_FILE
     echo "Action ID: $scaleGceDown"
-    task=$(curl -s -H "X-Auth-Token: $token" -H "Content-Type: application/json" -d "{}" http://$OPENSTACK_IP:8082/v1/environments/$env_id/actions/$scaleGceDown)
+    task=$(curl -s -H "X-Auth-Token: $token" -H "Content-Type: application/json" -d "{}" $MURANO_URL/v1/environments/$env_id/actions/$scaleGceDown)
 fi
 
 task_id=`echo $task | jq --raw-output ".task_id" 2> /dev/null`
@@ -99,7 +124,7 @@ echo "Waiting for task to complete.."
 
 # function that checks task complition. 0 if finish, 1 otherwise
 function finish_task {
-   result=$(curl -s -H "X-Auth-Token: $token" http://$OPENSTACK_IP:8082/v1/environments/$env_id/actions/$task_id)
+   result=$(curl -s -H "X-Auth-Token: $token" $MURANO_URL/v1/environments/$env_id/actions/$task_id)
    if [ $(echo $result | jq ".isException") == null ] ; then
        echo "1"
    else
@@ -111,7 +136,7 @@ function finish_task {
 while true; do
   stat=$(finish_task)
   if [ $stat == "0" ] ; then
-    result=$(curl -s -H "X-Auth-Token: $token" http://$OPENSTACK_IP:8082/v1/environments/$env_id/actions/$task_id)
+    result=$(curl -s -H "X-Auth-Token: $token" $MURANO_URL/v1/environments/$env_id/actions/$task_id)
     if [ $(echo $result | jq ".isException") == "false" ] ; then
        echo 0 > $AUTO_FLAG_FILE
        echo "Done"
@@ -124,4 +149,3 @@ while true; do
   fi
   sleep 1
 done
-
